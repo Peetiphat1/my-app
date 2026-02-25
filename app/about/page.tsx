1810cb892c35e30706b647bfd1a79d383c7058b2
Peetiphat <xxxx26425@gmail.com>
2026-02-25 11:29:34 +0700

    kitty

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+export default function Aboutpage() {
+    return (
+        <div className='flex justify-center items-center min-h-screen'>
+            <h1 className='text-[8rem] font-bold text-shadow-lg italic'>
+
+                About
+            </h1>
+        </div>
+    )
+}
